23d6dffb548296675530ebf2ed5504a867317164
Dani Carrasco <danartfun@gmail.com>
2025-09-01 14:51:55 +0000

    Initial Commit

---
+++ srv/risk-service.js
@@ -0,0 +1,120 @@
+const cds = require("@sap/cds");
+
+module.exports = cds.service.impl(async function () {
+    const { Risks, BusinessPartners } = this.entities;
+
+    this.after("READ", Risks, (data) => {
+        const risks = Array.isArray(data) ? data : [data];
+        risks.forEach((risk) => {
+            if (risk.impact >= 100000) {
+                risk.criticality = 1; // Alta criticidad (rojo)
+            } else {
+                risk.criticality = 2; // Media criticidad (amarillo)
+            }
+        });
+    });
+
+    this.before("CREATE", Risks, async (req) => {
+        // Simular que el título ya existe
+        if (req.data.title === "Duplicated Risk") {
+            throw new Error("UNIQUE_CONSTRAINT_VIOLATION");
+        }
+
+        if (!req.data.title) {
+            throw new Error("A risk must have a title.");
+        }
+    });
+
+    this.on("submitOrder", async (req) => {
+        const { book, amount } = req.data;
+        let { stock } = await db.read(Books, book, (b) => b.stock);
+
+        if (stock >= amount) {
+            await db.update(Books, book).with({ stock: (stock -= amount) });
+            return req.reply({ stock });
+        } else {
+            return req.error(409, `${amount} exceeds stock for book #${book}`);
+        }
+    });
+
+
+
+    this.on("error", (err, req) => {
+        switch (err.message) {
+            case "UNIQUE_CONSTRAINT_VIOLATION":
+                err.message = "The entry already exists.";
+                break;
+            default:
+                err.message =
+                    "An error occurred. Please retry. Technical error message: " +
+                    err.message;
+                break;
+        }
+    });
+
+    // Conectar al servicio externo
+    const BPsrv = await cds.connect.to("API_BUSINESS_PARTNER");
+
+    // Handler para leer BusinessPartners con API key
+    this.on("READ", BusinessPartners, async (req) => {
+        req.query.where("LastName <> '' and FirstName <> '' ");
+        return await BPsrv.tx(req).send({
+            query: req.query,
+            headers: {
+                apikey: process.env.apikey,
+            },
+        });
+    });
+
+    //### BEGIN OF INSERT
+    /**
+     * Event-handler on risks.
+     * Retrieve BusinessPartner data from the external API
+     */
+    this.on("READ", Risks, async (req, next) => {
+        /*
+        Check whether the request wants an "expand" of the business partner
+        As this is not possible, the risk entity and the business partner
+       entity are in different systems (SAP BTP and S/4 HANA Cloud),
+        if there is such an expand, remove it
+        */
+        if (!req.query.SELECT.columns) return next();
+        const expandIndex = req.query.SELECT.columns.findIndex(
+            ({ expand, ref }) => expand && ref[0] === "bp"
+        );
+        console.log(req.query.SELECT.columns);
+        if (expandIndex < 0) return next();
+        req.query.SELECT.columns.splice(expandIndex, 1);
+        if (
+            !req.query.SELECT.columns.find((column) =>
+                column.ref.find((ref) => ref == "bp_BusinessPartner")
+            )
+        ) {
+            req.query.SELECT.columns.push({ ref: ["bp_BusinessPartner"] });
+        }
+        /* Instead of carrying out the expand, issue a separate request for each business partner
+           This code could be optimized, instead of having n requests for n business partners, 
+           just one bulk request could be created */
+        try {
+            res = await next();
+            res = Array.isArray(res) ? res : [res];
+            await Promise.all(
+                res.map(async (risk) => {
+                    const bp = await BPsrv.tx(req).send({
+                        query: SELECT.one(this.entities.BusinessPartners)
+                            .where({ BusinessPartner: risk.bp_BusinessPartner })
+                            .columns(["BusinessPartner", "LastName", "FirstName"]),
+                        headers: {
+                            apikey: process.env.apikey,
+                        },
+                    });
+                    risk.bp = bp;
+                })
+            );
+        } catch (error) { }
+    });
+    //### END OF INSERT
+
+
+
+});
